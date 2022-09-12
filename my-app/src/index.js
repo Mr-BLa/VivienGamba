9191733885c1e456570d4c763ab39964b612b23b
Brice Laroche <laroche.brice@gmail.com>
2022-09-12 10:27:33 +0200

    structure

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,7 +1,14 @@
+// INDEX.JS 
+
+
+
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+// import { BrowserRouter, Routes, Route } from "react-router-dom"
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import '../src/css/index.css';
+import App from './components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
