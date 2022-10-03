77c4d5fb99f56d7abcfa6689136393694a8e45ab
Brice Laroche <laroche.brice@gmail.com>
2022-10-03 09:39:39 +0200

    header.js + .css : animation delay sur dropdown

--- my-app/src/components/header.js
+++ my-app/src/components/header.js
@@ -14,20 +14,66 @@ export default function Header() {
     /* Quand l'user clique sur le bouton, toggle entre: "hide" et "show" le dropdown*/
     function dropDown() {
         document.getElementById("myDropdown").classList.toggle("show");
-        // document.getElementsByClassName("header--menulogo").style.display="none";
+
+        /* les delays sur les link du dropdown */
+        document.getElementById("dropdownLink1").classList.toggle("delay1")
+        document.getElementById("dropdownLink2").classList.toggle("delay2")
+        document.getElementById("dropdownLink3").classList.toggle("delay3")
+        document.getElementById("dropdownLink4").classList.toggle("delay4")
+        document.getElementById("dropdownLink5").classList.toggle("delay5")
+        document.getElementById("dropdownLink6").classList.toggle("delay6")
+        document.getElementById("dropdownLink7").classList.toggle("delay7")
+        document.getElementById("dropdownLink8").classList.toggle("delay8")
     }
 
     // Ferme le menu dropdown, si l'user clique à l'éxtérieur de celui-ci
     window.onclick = function(event) {
         if (!event.target.matches('.dropbtn')) {
             let dropdowns = document.getElementsByClassName("dropdown-content");
+            let links = document.getElementsByClassName("link--menu");
             let i;
+
+            /* Suppression du dropdown */
             for (i = 0; i < dropdowns.length; i++) {
                 let openDropdown = dropdowns[i];
+
                 if (openDropdown.classList.contains('show')) {
                     openDropdown.classList.remove('show');
                 }
             }
+
+            /* Suppression des delay */
+            for (i = 0; i < links.length; i++) {
+                let openLinks = links[i]
+
+                if (openLinks.classList.contains('delay1')) {
+                    openLinks.classList.remove('delay1');
+                }
+                if (openLinks.classList.contains('delay2')) {
+                    openLinks.classList.remove('delay2');
+                }
+                if (openLinks.classList.contains('delay3')) {
+                    openLinks.classList.remove('delay3');
+                }
+                if (openLinks.classList.contains('delay4')) {
+                    openLinks.classList.remove('delay4');
+                }
+                if (openLinks.classList.contains('delay5')) {
+                    openLinks.classList.remove('delay5');
+                }
+                if (openLinks.classList.contains('delay6')) {
+                    openLinks.classList.remove('delay6');
+                }
+                if (openLinks.classList.contains('delay7')) {
+                    openLinks.classList.remove('delay7');
+                }
+                if (openLinks.classList.contains('delay8')) {
+                    openLinks.classList.remove('delay8');
+                }
+            }
+                
+                
+            
         }
     }
 
@@ -52,14 +98,14 @@ export default function Header() {
                             onClick={dropDown}
                             />
                         <div id="myDropdown" className="dropdown-content">
-                            <Link to="/apropos" className="link--menu">À propos</Link>
-                            <Link to="/sculptures" className="link--menu">Sculptures</Link>
-                                <Link to="/sculptures/saintElient" className="link--menu link--sous-menu">Saint Elient</Link>
-                                <Link to="/sculptures/saintPer" className="link--menu link--sous-menu">Saint Per</Link>
-                                <Link to="/sculptures/saintGuillaumePinchon" className="link--menu link--sous-menu">Saint Guillaume Pinchon</Link>
-                                <Link to="/sculptures/saintTenenan" className="link--menu link--sous-menu">Saint Tenenan</Link>
-                                <Link to="/sculptures/saintElouan" className="link--menu link--sous-menu">Saint Elouan</Link>
-                            <Link to="/contact" className="link--menu">Contact</Link>
+                            <Link to="/apropos" id="dropdownLink1" className="link--menu ">À propos</Link>
+                            <Link to="/sculptures" id="dropdownLink2" className="link--menu ">Sculptures</Link>
+                                <Link to="/sculptures/saintElient" id="dropdownLink3" className="link--menu link--sous-menu ">Saint Elient</Link>
+                                <Link to="/sculptures/saintPer" id="dropdownLink4" className="link--menu link--sous-menu ">Saint Per</Link>
+                                <Link to="/sculptures/saintGuillaumePinchon" id="dropdownLink5" className="link--menu link--sous-menu ">Saint Guillaume Pinchon</Link>
+                                <Link to="/sculptures/saintTenenan" id="dropdownLink6" className="link--menu link--sous-menu ">Saint Tenenan</Link>
+                                <Link to="/sculptures/saintElouan" id="dropdownLink7" className="link--menu link--sous-menu ">Saint Elouan</Link>
+                            <Link to="/contact" id="dropdownLink8" className="link--menu ">Contact</Link>
                         </div>
                     </div>
                 </nav>
